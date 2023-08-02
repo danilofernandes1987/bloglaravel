49636de8e5ea79e494c15969f394855bc9dca46e
Danilo Fernandes da Silva <danilofernandes1987@gmail.com>
2023-08-01 21:30:15 -0300

    banco de dados

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 100);
+            $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password',100);
+            $table->boolean('moderador');
+            $table->string('avatar',150)->nullable();
+            $table->string('link_instagram', 150)->nullable();
+            $table->string('link_facebook', 150)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
